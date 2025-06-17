b0b34ea903d97626d0665a073db50183f707c45d
Bailic25 <flowtunder@gmail.com>
2025-06-17 14:38:41 +0700

    Bagus Harimukti, controller data baru :)

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -9,10 +9,22 @@ class DataController extends Controller
 {
     public function index()
     {
-        $kerjasamas = Kerjasama::with(['dokumen', 'mitra', 'pemohon.jurusan'])->get();
+        $kerjasamas = Kerjasama::with([
+            'dokumen',
+            'mitra',
+            'pemohon.jurusan',
+            'pemohon.bidangs',
+            'ajuan',
+            'bidang_kerjasama',
+        ])->get();
+        
+        // Ambil Bidang Kerjasama yang digunakan oleh mitra pada kerjasama yang terdaftar
+        $usedBidangIds = $kerjasamas->pluck('id_bidang')->unique();
+        $bidangKerjasamas = \App\Models\BidangKerjasama::whereIn('id_bidang', $usedBidangIds)->get();
         
         return view('datakerjasama', [
-            'kerjasamas' => $kerjasamas
+            'kerjasamas' => $kerjasamas,
+            'bidangKerjasamas' => $bidangKerjasamas,
         ]);
     }
 }
